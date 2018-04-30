7889747bccb6f5ecdcf9df8b671ddc4112761d93
NorbertWychowski <norbertwychowski@gmail.com>
2018-04-30 19:52:20 +0200

    v3

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,10 +2,6 @@ class StaticController < ApplicationController
   include ApplicationHelper
 
   def index
-    respond_to do |format|
-      format.html
-      format.js
-    end
     @weather = get_weather
   end
 end
