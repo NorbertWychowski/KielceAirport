35911e57e238806f2494a17298724234c7a9d4d4
NorbertWychowski <norbertwychowski@gmail.com>
2018-05-02 17:09:57 +0200

    v4.2

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,7 @@ class StaticController < ApplicationController
   include ApplicationHelper
 
   def index
+    puts Rails.configuration.action_mailer.smtp_settings
     @weather = get_weather
   end
 end
